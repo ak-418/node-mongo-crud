4f08ec6ba8c2ffdb4c9fea8dc93a9c0e89e38192
Amritha K <amrithakrishnan94@gmail.com>
2021-12-30 16:15:46 +0530

    Add few comments

--- app.js
+++ app.js
@@ -11,23 +11,28 @@ async function main() {
 
   try {
     await client.connect();
-
+    // Populate the db
     const results = await circulationRepo.loadData(data);
     assert.equal(results.insertedCount, data.length);
 
+    // Fetch all data
     const getData = await circulationRepo.get();
     assert.equal(getData.length, data.length);
 
+    // Filter data
     const filterData = await circulationRepo.get({ Newspaper: getData[4].Newspaper });
     assert.deepEqual(filterData[0], getData[4]);
 
+    // Fetch by limit
     const limitData = await circulationRepo.get({}, 4);
     assert.deepEqual(limitData.length, 4);
 
+    // Get data by ID
     const id = getData[0]._id.toString();
     const byId = await circulationRepo.getById(id);
     assert.deepEqual(getData[0], byId);
 
+    // Insert new item
     const newItem = {
       Newspaper: 'My Own thing',
       'Daily Circulation, 2004': 2192,
@@ -40,7 +45,6 @@ async function main() {
 
     const insertedItem = await circulationRepo.addItem(newItem);
     const insertedItemById = await circulationRepo.getById(insertedItem[0]._id);
-
     assert.deepEqual(newItem, insertedItemById);
   } catch (error) {
     console.log({ error });
